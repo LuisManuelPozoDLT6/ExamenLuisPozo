bc504becd52bf6a3b9b1f83aaf1f7ef56e5266c4
LuisManuelPozoDLT6 <20203tn047@utez.edu.mx>
2021-06-25 19:10:44 -0500

    Examen2

---
+++ js/Mensaje.js
@@ -0,0 +1,4 @@
+const searchMessage = () =>{
+    let search = document.getElementById("busqueda").value;
+    alert(`Lo sentimos, la película "${search}" no esta disponible por el momento, intentelo más tarde` )
+}
